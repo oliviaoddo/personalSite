2283416dec4d588e61785ea25bc6353a05f2063e
Olivia Oddo <olivia.oddo@gmail.com>
2017-01-11 18:49:50 -0800

    resume with lightbox and download button

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,38 +1,40 @@
-//Problem: User when clicking on image goes to a dead end
-//Solution: Create an overlay with the large image - Lightbox
-var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-var $caption = $("<p></p>");
+// Problem: User when clicking on image goes to a dead end
+// Solution: Create an overlay with the large image - Lightbox
 
-//An image to overlay
-$overlay.append($image);
+console.log("hello");
+$('#popup').hide();
 
-//A caption to overlay
-$overlay.append($caption);
-
-//Add overlay
-$("body").append($overlay);
 
 $(document).on('click', '#resume', function(event) {
     event.preventDefault();
-    console.log(this);
-    var imageLocation = $(this).attr("href");
-    console.log(imageLocation);
-    //Update overlay with the image linked in the link
-    $image.attr("src", imageLocation);
-    console.log($image);
-  
-    //Show the overlay.
+    // console.log(this);
+    // var imageLocation = $(this).attr("href");
+    // console.log(imageLocation);
+    // //Update overlay with the image linked in the link
+    // $image.attr("src", imageLocation);
+    // console.log($image);
+    // // //Get child's alt attribute and set caption
+    // // var captionText = $(this).children("img").attr("alt");
+    // // $caption.text(captionText);
+    var $overlay = $('<div id="overlay"></div>'); 
+    var $icon = $("<div id='popup'><div id='downloadicon'><a href='images/webResume.pdf' download><i style='color:#fff' class='fa fa-download fa-2x' aria-hidden='true'></i></a></div><img src='images/webResume' alt='Olivia Oddo Resume'></div>");
+    // var $image = $("<img src='images/webResume' alt='Olivia Oddo Resume'>");
+    // var $popUp = $("#popup");
+    // console.log($popUp);
+    // $icon.append($image);
+    $overlay.append($icon);
+    $("body").append($overlay);
     $overlay.show();
-  
-    //Get child's alt attribute and set caption
-    var captionText = $(this).children("img").attr("alt");
-    $caption.text(captionText);
+    // $('div#popUp').show();
+
+        //When overlay is clicked
+    $overlay.click(function(){
+      //Hide the overlay
+      $overlay.hide();
+    });
 });
 
-
-//When overlay is clicked
-$overlay.click(function(){
-  //Hide the overlay
-  $overlay.hide();
-});
+        // <div id='downloadicon'>
+        //     <a href='images/webResume.pdf' download><i style='color:#fff' class='fa fa-download fa-2x' aria-hidden='true'></i></a>
+        // </div>
+        // <img src="images/webResume" alt="Olivia Oddo Resume">
